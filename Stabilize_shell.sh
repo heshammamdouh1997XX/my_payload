cec23ceae71350e6123e0ff4e5867a1e177f9f8c
heshammamdouh1997XX <58068707+heshammamdouh1997XX@users.noreply.github.com>
2021-06-11 08:06:59 -0400

    Update Stabilize_shell.sh

--- Stabilize_shell.sh
+++ Stabilize_shell.sh
@@ -3,8 +3,14 @@
 source "/opt/pmp/functions.sh"
 
 hide_guake
-call_cmd "python -c 'import pty; pty.spawn(\"/bin/bash\")'"
+call_cmd "python -c 'import pty; pty.spawn("/bin/sh")'"
 ctrl Z
 call_cmd "stty raw -echo"
+
 call_cmd "fg"
 call_cmd "export TERM=xterm"
+
+
+OR
+
+incmd "/usr/bin/script -qc /bin/bash /dev/null"
